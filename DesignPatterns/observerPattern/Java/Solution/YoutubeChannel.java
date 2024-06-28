168eee479d8c45803a197c3ee1799b903d82c229
Badisa Naveen <89301294+naveen5655@users.noreply.github.com>
2024-06-28 20:47:28 +0530

    Create YoutubeChannel.java

---
+++ DesignPatterns/observerPattern/Java/Solution/YoutubeChannel.java
@@ -0,0 +1,41 @@
+package naveen.Solution;
+
+import java.util.*;
+
+// Subject
+public class YoutubeChannel {
+	
+	private final String name;
+	private ArrayList<Subscriber> subscribers;
+	private ArrayList<String> videos;
+	
+	
+	YoutubeChannel(String name){
+		this.name = name;
+		this.subscribers = new ArrayList<>();
+		this.videos = new ArrayList<>();
+		
+	}
+	
+	public void subsribe(Subscriber subscriber){
+		this.subscribers.add(subscriber);
+		
+	}
+	
+	public void unSubscribe(Subscriber subscriber) {
+		this.subscribers.remove(subscriber);
+		
+	}
+	
+	public void addVideo(String name) {
+		this.videos.add(name);
+	    this.subscribers.forEach(subscriber -> subscriber.notify(
+				"New Video added with title:" + name));
+	}
+
+	public ArrayList<Subscriber> getSubscribers(){
+		return this.subscribers;
+	}
+	
+
+}
